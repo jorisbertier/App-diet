782a30663aee65c7cec4c0414b591c4904ad5923
Joris Bertier <joris.bertier@gmail.com>
2024-10-09 19:56:04 +0200

    create component NutritionStatCard

--- screens/[id].tsx
+++ screens/[id].tsx
@@ -1,9 +1,15 @@
 import { ThemedText } from "@/components/ThemedText"
 import { router } from "expo-router"
-import { Image, Pressable, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, View, Text } from "react-native";
 import { useNavigation } from "expo-router";
+import Row from "@/components/Row";
+import useThemeColors from "@/hooks/UseThemeColors";
+import NutritionStatCard from "@/components/Screens/Search/NutritionStatCard";
+
+
 
 export default function DetailsFood() {
+    const colors = useThemeColors()
 
     const navigation = useNavigation();
 
@@ -18,7 +24,38 @@ export default function DetailsFood() {
             </Pressable>
         </View>
         <View style={styles.header}>
-            <ThemedText>Voici ma page details food</ThemedText>
+            <Row>
+                <View style={styles.wrapperBlock}>
+                    <ThemedText style={styles.block}>Fruit</ThemedText>
+                    <ThemedText style={styles.block}>250g</ThemedText>
+                </View>
+            </Row>
+            <Row style={styles.wrapperTitle}>
+                <ThemedText variant="title1">Mandarin orange</ThemedText>
+                <ThemedText variant="title1">Good for diet - 187kcal</ThemedText>
+            </Row>
+            <View style={styles.container}>
+                <Row gap={10}>
+                    <NutritionStatCard
+                        nutri={'protein'}
+                        quantity={51}
+                        unit={'g'}
+                        backgroundcolor={'#000000'}
+                    />
+                    <NutritionStatCard
+                        nutri={'carbs'}
+                        quantity={51}
+                        unit={'g'}
+                        backgroundcolor={'#FF8400'}
+                    />
+                    <NutritionStatCard
+                        nutri={'fat'}
+                        quantity={88}
+                        unit={'g'}
+                        backgroundcolor={'#4A83D4'}
+                    />
+                </Row>
+            </View> 
         </View>
     </>
     )
@@ -28,8 +65,9 @@ const styles = StyleSheet.create({
     header: {
         paddingHorizontal: 12,
         paddingBottom: 8,
-        backgroundColor: 'red',
-        flex: 1/3
+        flex: 1/3,
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40
     },
     banner: {
         position: 'relative',
@@ -55,5 +93,30 @@ const styles = StyleSheet.create({
     icon: {
         width: 25,
         height: 25
+    },
+    wrapperBlock: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 10,
+        width: '100%',
+        marginTop: 20
+
+    },
+    block: {
+        borderWidth: 1,
+        borderBlockColor: 'black',
+        backgroundColor: 'gray',
+        padding: 6
+    },
+    wrapperTitle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+        flexDirection: 'column'
+    },
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 20,
     }
 })
