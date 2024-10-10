6697373a2e1f6a27f7bb9886c44291d5a27dfe02
Joris Bertier <joris.bertier@gmail.com>
2024-10-10 14:43:05 +0200

    get date and add calendar

--- screens/dashboard.tsx
+++ screens/dashboard.tsx
@@ -1,17 +1,32 @@
 import Row from "@/components/Row";
 import { ThemedText } from "@/components/ThemedText";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Button } from "react-native";
+import RNDateTimePicker, { DateTimePicker, DateTimePickerEvent} from "@react-native-community/datetimepicker";
+import { useState } from "react";
 
 export default function Dashboard() {
 
-    const date = new Date();
+    // const date = new Date();
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+    // let day = date.getDate();
+    // let month = date.getMonth() + 1;
+    // let year = date.getFullYear();
     
-    let currentDate = `${day}-${month}-${year}`;
-    console.log(currentDate)
+    // let currentDate = `${day}-${month}-${year}`;
+    // console.log(currentDate)
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedDate, setSelectedDate]= useState<Date>(new Date())
+    
+    const setDate = (event: DateTimePickerEvent, date: Date) => {
+        if(date) {
+            setSelectedDate(date);
+            setIsOpen(false)
+        }
+    };
+
+    const handleOpenCalendar = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <View style={styles.header}>
@@ -21,7 +36,17 @@ export default function Dashboard() {
                     <Image source={require('@/assets/images/navbar/home.png')} style={styles.next} />
                 </View>
                 <View>
-                    <ThemedText>{currentDate === currentDate ? "Ajourd'hui" : currentDate}</ThemedText>
+                    {/* <ThemedText>{currentDate === currentDate ? "Ajourd'hui" : currentDate}</ThemedText> */}
+                    
+                    {/* <RNDateTimePicker value={new Date()} timeZoneName={'Europe/Paris'} display="spinner" /> */}
+                    <Button
+                        onPress={handleOpenCalendar}
+                        title="Learn More"
+                        color="#841584"
+                        accessibilityLabel="Learn more about this purple button"
+                        />
+                        {isOpen && (<RNDateTimePicker onChange={setDate} value={new Date()} timeZoneName={'Europe/Paris'}/>)}
+                        <ThemedText>{selectedDate.toLocaleDateString('fr-FR')}</ThemedText>
                 </View>
                 <View>
                     <Image source={require('@/assets/images/navbar/home.png')} style={styles.next} />
